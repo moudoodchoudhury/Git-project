4239e62d69e70c1ba06615623bbcbe33d6dca967
Moudood Choudhury <moudood_chy@yahoo.com>
2019-01-15 21:06:21 -0500

    Second user function coding

--- src/MyApplication/UserFunction.java
+++ src/MyApplication/UserFunction.java
@@ -3,9 +3,11 @@ package MyApplication;
 public class UserFunction {
 
 	public static void main(String[] args) {
-		
-		
+
 		System.out.println("user name: moudud");
 	}
 
+	public void add(String name, int age) {
+		System.out.println("My name is:" + name);
+	}
 }
